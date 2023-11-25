323c40644066ad6b3da29939a17a5f40739631e7
Breno <brenogiovanniolintho@gmail.com>
2023-11-25 12:53:26 -0300

    Register Functionality

--- components/AuthForm/AuthForm.tsx
+++ components/AuthForm/AuthForm.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
+import axios from 'axios';
 
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
@@ -50,7 +51,7 @@ export default function AuthForm() {
         setIsLoading(true);
 
         if(variant === 'REGISTER'){
-            // axios register
+            axios.post('/api/register', values)
         }
 
         if(variant === 'LOGIN'){
